86013fec017f105971dc804b21f6dac62401a5cd
JoshuaMac40 <53539972+JoshuaMac40@users.noreply.github.com>
2019-12-24 16:53:18 +0530

    Add files via upload

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '30b84b39b8c369b02079';
+    this.client_secret = '7a05747ef492b0eabf5a1517a3b295a4aa7ba584';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
